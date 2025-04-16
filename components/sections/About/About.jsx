5eb2d9620619d70f510a82952b25962e67e869f0
Mohssine Chedgane <chedganemouhssine@gmail.com>
2025-04-16 20:00:29 +0100

    fixing about section

--- components/sections/About/About.jsx
+++ components/sections/About/About.jsx
@@ -8,29 +8,34 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <section className={styles.about}>
-      <div className={styles.abimage}>
-        <img src="/assets/images/aboutMe.png" alt="me" />
+      <div className={styles.left}>
+        <div className={styles.abimage}>
+          <img src="/assets/images/aboutMe.png" alt="me" />
+        </div>
       </div>
-      <BlurryButton label="🧐 About me " />
-      <div className={styles.about_content}>
-        <div className={styles.p}>
-          <p>
-            👋 Me chamo Washington Henrique Fernandes de Sousa, mas pode me
-            chamar apenas de Henrique. Prazer!
-          </p>
-          <p>
-            👨‍💻 Há mais de 2 anos desenvolvendo e programando interfaces com
-            JavaScript, React JS e Typescript.
-          </p>
-          <p>
-            🎓 Graduado em Análise e Desenvolvimento de Sistemas pela Fatec de
-            São José dos Campos
-          </p>
-          <p>
-            💡 Interesses em desenvolvimento Front-end com React, React Native,
-            VueJS e UX/UI Design.
-          </p>
-          <p>🚀 Tentando ser um pouquinho melhor do que ontem todos dias.</p>
+
+      <div className={styles.right}>
+        <BlurryButton label="🧐 About me " />
+        <div className={styles.about_content}>
+          <div className={styles.p}>
+            <p>
+              👋 Me chamo Washington Henrique Fernandes de Sousa, mas pode me
+              chamar apenas de Henrique. Prazer!
+            </p>
+            <p>
+              👨‍💻 Há mais de 2 anos desenvolvendo e programando interfaces com
+              JavaScript, React JS e Typescript.
+            </p>
+            <p>
+              🎓 Graduado em Análise e Desenvolvimento de Sistemas pela Fatec de
+              São José dos Campos
+            </p>
+            <p>
+              💡 Interesses em desenvolvimento Front-end com React, React
+              Native, VueJS e UX/UI Design.
+            </p>
+            <p>🚀 Tentando ser um pouquinho melhor do que ontem todos dias.</p>
+          </div>
         </div>
       </div>
     </section>

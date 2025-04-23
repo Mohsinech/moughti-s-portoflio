c27cc2c12d79290fd11187b2654e81d8133eb201
abdelmoughite <abdel.hm4@gmail.com>
2025-04-23 21:33:30 +0100

    contact

--- components/sections/contact/Contact.jsx
+++ components/sections/contact/Contact.jsx
@@ -8,6 +8,31 @@ const Contact = () => {
       <div className={styles.first_pj}>
         <BlurryButton label="🧐 Contact me " />
       </div>
+      <div className={styles["form-container"]}>
+        <form className={styles.form}>
+          <div className={styles["form-group"]}>
+            <label htmlFor="email">Email</label>
+            <input required name="email" id="email" type="text" />
+          </div>
+          <div className={styles["form-group"]}>
+            <label htmlFor="Subject">Subject</label>
+            <input required name="Subject" id="Subject" type="text" />
+          </div>
+          <div className={styles["form-group"]}>
+            <label htmlFor="textarea">How Can We Help You?</label>
+            <textarea
+              required
+              cols="50"
+              rows="10"
+              id="textarea"
+              name="textarea"
+            ></textarea>
+          </div>
+          <button type="submit" className={styles["form-submit-btn"]}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
